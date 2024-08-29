97e8f3efe7af9c3603e85f2b2ae66e9003d317b3
Sanskar Maharwade <sanskarsmacbook@Sanskars-MacBook-Air.local>
2024-08-29 12:12:39 +0530

    key

--- src/app/search/[searchTerm]/page.jsx
+++ src/app/search/[searchTerm]/page.jsx
@@ -1,22 +1,33 @@
-import Results from '@/components/Results';
-
 export default async function SearchPage({ params }) {
-  const seachTerm = params.searchTerm;
-  const res = await fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${seachTerm}&language=en-US&page=1&include_adult=false`
-  );
+  const searchTerm = params.searchTerm;
+
+  const API_KEY = process.env.API_KEY;
   if (!API_KEY) {
     throw new Error('Missing API_KEY environment variable');
-  }  
-  const data = await res.json();
-  const results = data.results;
-  return (
-    <div>
-    {results && results.length === 0 && (
-      <h1 className='text-center pt-6'>No results found</h1>
-    )}
-    {results && results.length > 0 && <Results results={results} />}
-  </div>
-  
-  );
+  }
+
+  try {
+    const res = await fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${searchTerm}&language=en-US&page=1&include_adult=false`
+    );
+
+    if (!res.ok) {
+      throw new Error(`Failed to fetch data: ${res.status} ${res.statusText}`);
+    }
+
+    const data = await res.json();
+    const results = data.results;
+
+    return (
+      <div>
+        {results && results.length === 0 && (
+          <h1 className="text-center pt-6">No results found</h1>
+        )}
+        {results && results.length > 0 && <Results results={results} />}
+      </div>
+    );
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw new Error('Failed to fetch data');
+  }
 }
