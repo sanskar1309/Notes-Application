ec56467b9c57272e0dac64bace4a1c4cb4899a64
Sanskar Maharwade <sanskarsmacbook@Sanskars-MacBook-Air.local>
2024-08-29 13:25:26 +0530

    image'

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,7 +1,8 @@
+import Image from 'next/image';
 export default function loading() {
   return (
     <div className='flex justify-center mt-16'>
-      <img className='h-52' src='spinner.svg' alt='loading...' />
+      <Image className='h-52' src='spinner.svg' alt='loading...' ></Image>
     </div>
   );
 }
